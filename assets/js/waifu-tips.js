184192404d87c3c33f444bdb58fc1ec587230161
lidong(YLD/Winter) <aiyld@hotmail.com>
2022-12-09 11:14:00 +0800

    Init

---
+++ assets/js/waifu-tips.js
@@ -0,0 +1,266 @@
+String.prototype.render = function (context) {
+    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+
+    return this.replace(tokenReg, function (word, slash1, token, slash2) {
+        if (slash1 || slash2) {  
+            return word.replace('\\', '');
+        }
+
+        var variables = token.replace(/\s/g, '').split('.');
+        var currentObject = context;
+        var i, length, variable;
+
+        for (i = 0, length = variables.length; i < length; ++i) {
+            variable = variables[i];
+            currentObject = currentObject[variable];
+            if (currentObject === undefined || currentObject === null) return '';
+        }
+        return currentObject;
+    });
+};
+
+var re = /x/;
+console.log(re);
+re.toString = function() {
+    showMessage('Haha, you opened the console, do you want to see my secret?', 5000, true);
+    return '';
+};
+
+$(document).on('copy', function (){
+    showMessage('What have you copied, remember to add the source when reprinting', 5000, true);
+});
+
+$('.waifu-tool .fui-home').click(function (){
+    //window.location = 'https://www.fghrsh.net/';
+    window.location = window.location.protocol+'//'+window.location.hostname+'/'
+});
+
+$('.waifu-tool .fui-eye').click(function (){
+    loadOtherModel();
+});
+
+$('.waifu-tool .fui-chat').click(function (){
+    showHitokoto();
+});
+
+$('.waifu-tool .fui-user').click(function (){
+    loadRandModel();
+});
+
+$('.waifu-tool .fui-cross').click(function (){
+    sessionStorage.setItem('waifu-dsiplay', 'none');
+    showMessage('May you one day be able to reunite with important people', 1300, true);
+    window.setTimeout(function() {$('.waifu').hide();}, 1300);
+});
+
+$('.waifu-tool .fui-photo').click(function (){
+    showMessage("Well done, isn't it cute?", 5000, true);
+    window.Live2D.captureName = 'Pio.png';
+    window.Live2D.captureFrame = true;
+});
+
+(function (){
+    var text;
+    //var SiteIndexUrl = 'https://www.fghrsh.net/';  // 手动指定主页
+    var SiteIndexUrl = window.location.protocol+'//'+window.location.hostname+'/';  // 自动获取主页
+    
+    if (window.location.href == SiteIndexUrl) {      // 如果是主页
+        var now = (new Date()).getHours();
+        if (now > 23 || now <= 5) {
+            text ="Are you a night owl? I still don't sleep so late, why don't I get up tomorrow";
+        } else if (now > 5 && now <= 7) {
+            text ="Good morning! A day's plan lies in the morning, a beautiful day is about to begin";
+        } else if (now > 7 && now <= 11) {
+            text ="Good morning! Work is going well, don't sit for a long time, get up and move around more! ";
+        } else if (now > 11 && now <= 14) {
+            text ="It's noon, I've worked all morning, now it's lunch time! ";
+        } else if (now > 14 && now <= 17) {
+            text ="It's easy to get sleepy in the afternoon, have you completed today's exercise goal? ";
+        } else if (now > 17 && now <= 19) {
+            text ="It's evening! The sunset scenery outside the window is very beautiful, the most beautiful is the sunset red~";
+        } else if (now > 19 && now <= 21) {
+            text ="Good evening, how are you doing today? ";
+        } else if (now > 21 && now <= 23) {
+            text ="It's so late, go to bed early, good night~";
+        } else {
+            text ="Hi~ come and tease me! ";
+        }
+    } else {
+        if(document.referrer !== ''){
+            var referrer = document.createElement('a');
+            referrer.href = document.referrer;
+            var domain = referrer.hostname.split('.')[1];
+            if (window.location.hostname == referrer.hostname) {
+                text = 'Welcome to read <span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            } else if (domain == 'baidu') {
+                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
+            } else if (domain == 'so') {
+                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
+            } else if (domain == 'google') {
+                text = 'Hello! Friends from google search<br>Welcome to read<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            } else {
+                text = 'Hello! Friends from <span style="color:#0099cc;">' + referrer.hostname + '</span>';
+            }
+        } else {
+            text = 'Welcome to read <span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+        }
+    }
+    showMessage(text, 6000);
+})();
+
+//window.hitokotoTimer = window.setInterval(showHitokoto,30000);
+/* 检测用户活动状态，并在空闲时 定时显示一言 */
+var getActed = false;
+window.hitokotoTimer = 0;
+var hitokotoInterval = false;
+
+$(document).mousemove(function(e){getActed = true;}).keydown(function(){getActed = true;});
+setInterval(function() { if (!getActed) ifActed(); else elseActed(); }, 1000);
+
+function ifActed() {
+    if (!hitokotoInterval) {
+        hitokotoInterval = true;
+        hitokotoTimer = window.setInterval(showHitokoto, 30000);
+    }
+}
+
+function elseActed() {
+    getActed = hitokotoInterval = false;
+    window.clearInterval(hitokotoTimer);
+}
+
+function showHitokoto(){
+	/* 增加 hitokoto.cn API */
+    $.getJSON('https://v1.hitokoto.cn',function(result){
+        // var text = '<span style="color:#0099cc;">{source}</span>，是来自<span style="color:#0099cc;">{from}</span>';
+        // text = text.render({source: result.from, creator: result.creator});
+        showMessage(result.hitokoto, 5000);
+        // window.setTimeout(function() {showMessage(text, 3000);}, 5000);
+    });
+	/*
+	$.getJSON('https://api.fghrsh.net/hitokoto/rand/?encode=jsc&uid=3335',function(result){
+        var text = '这句一言出处是 <span style="color:#0099cc;">『{source}』</span>，是 <span style="color:#0099cc;">FGHRSH</span> 在 {date} 收藏的！';
+        text = text.render({source: result.source, date: result.date});
+        showMessage(result.hitokoto, 5000);
+        window.setTimeout(function() {showMessage(text, 3000);}, 5000);
+    });
+	*/
+}
+
+function showMessage(text, timeout, flag){
+    if(flag || sessionStorage.getItem('waifu-text') === '' || sessionStorage.getItem('waifu-text') === null){
+        if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+        //console.log(text);
+        
+        if(flag) sessionStorage.setItem('waifu-text', text);
+        
+        $('.waifu-tips').stop();
+        $('.waifu-tips').html(text).fadeTo(200, 1);
+        if (timeout === undefined) timeout = 5000;
+        hideMessage(timeout);
+    }
+}
+
+function hideMessage(timeout){
+    $('.waifu-tips').stop().css('opacity',1);
+    if (timeout === undefined) timeout = 5000;
+    window.setTimeout(function() {sessionStorage.removeItem('waifu-text')}, timeout);
+    $('.waifu-tips').delay(timeout).fadeTo(200, 0);
+}
+
+function initModel(waifuPath){
+    
+    if (waifuPath === undefined) waifuPath = '';
+    var modelId = localStorage.getItem('modelId');
+    var modelTexturesId = localStorage.getItem('modelTexturesId');
+    
+    if (modelId == null) {
+        
+        /* 首次访问加载 指定模型 的 指定材质 */
+        
+        var modelId = 2;            // 模型 ID
+        var modelTexturesId = 36    // 材质 ID
+        
+    } loadModel(modelId, modelTexturesId);
+	
+	$.ajax({
+        cache: true,
+        url: waifuPath+'waifu-tips.json?t=1123',
+        dataType: "json",
+        success: function (result){
+            $.each(result.mouseover, function (index, tips){
+                $(document).on("mouseover", tips.selector, function (){
+                    var text = tips.text;
+                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                    text = text.render({text: $(this).text()});
+                    showMessage(text, 3000);
+                });
+            });
+            $.each(result.click, function (index, tips){
+                $(document).on("click", tips.selector, function (){
+                    var text = tips.text;
+                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                    text = text.render({text: $(this).text()});
+                    showMessage(text, 3000, true);
+                });
+            });
+            $.each(result.seasons, function (index, tips){
+                var now = new Date();
+                var after = tips.date.split('-')[0];
+                var before = tips.date.split('-')[1] || after;
+                
+                if((after.split('/')[0] <= now.getMonth()+1 && now.getMonth()+1 <= before.split('/')[0]) && 
+                   (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])){
+                    var text = tips.text;
+                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                    text = text.render({year: now.getFullYear()});
+                    showMessage(text, 6000, true);
+                }
+            });
+        }
+    });
+}
+
+function loadModel(modelId, modelTexturesId){
+    localStorage.setItem('modelId', modelId);
+    if (modelTexturesId === undefined) modelTexturesId = 0;
+    localStorage.setItem('modelTexturesId', modelTexturesId);
+    loadlive2d('live2d', 'https://api.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+}
+
+function loadRandModel(){
+    var modelId = localStorage.getItem('modelId');
+    var modelTexturesId = localStorage.getItem('modelTexturesId');
+    
+    var modelTexturesRandMode = 'rand';     // 可选 'rand'(随机), 'switch'(顺序)
+    
+    $.ajax({
+        cache: false,
+        url: 'https://api.fghrsh.net/live2d/'+modelTexturesRandMode+'_textures/?id='+modelId+'-'+modelTexturesId,
+        dataType: "json",
+        success: function (result){
+            if (result.textures['id'] == 1 && (modelTexturesId == 1 || modelTexturesId == 0)) {
+                showMessage("I don't have any other clothes yet", 3000, true);
+            } else {
+                showMessage('Do my new clothes look good?', 3000, true);
+            }
+            loadModel(modelId, result.textures['id']);
+        }
+    });
+}
+
+function loadOtherModel(){
+    var modelId = localStorage.getItem('modelId');
+    
+    var modelTexturesRandMode = 'switch';     // 可选 'rand'(随机), 'switch'(顺序)
+    
+    $.ajax({
+        cache: false,
+        url: 'https://api.fghrsh.net/live2d/'+modelTexturesRandMode+'/?id='+modelId,
+        dataType: "json",
+        success: function (result){
+            loadModel(result.model['id']);
+            showMessage(result.model['message'], 3000, true);
+        }
+    });
+}
